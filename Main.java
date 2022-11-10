2a605deba54a416d0871d3ba416570bb23a4cedf
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2022-11-10 10:27:41 +0900

    間違ったコミットメッセージ

--- Main.java
+++ Main.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class Main {
 	public static void main (String args[]){
 		Scanner sc = new Scanner(System.in);
+
+    for (int i=0; i<2; i++){		
 	  double height = sc.nextDouble();
 	  double weight = sc.nextDouble();	  
 	  double bmi = weight/(height*height);
 	  System.out.println((Math.floor(bmi*10000))/100);
-
+           }
 	  sc.close();
 	}
 }
